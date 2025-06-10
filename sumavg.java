73401486e078991e0dddedc79cff74aa98eee3f1
245122749053 <245122749053@mvsrec.edu.in>
2025-06-10 15:44:49 +0530

    Add files via upload

---
+++ sumavg.java
@@ -0,0 +1,16 @@
+public class sumavg {
+    public static void main(String[] args) {
+        int sum = 0;
+        int count = 10;
+
+        for (int i = 1; i <= count; i++) {
+            sum += i;
+        }
+
+        double average = (double) sum / count;
+
+        System.out.println("Sum of first 10 natural numbers: " + sum);
+        System.out.println("Average of first 10 natural numbers: " + average);
+    }
+}
+
